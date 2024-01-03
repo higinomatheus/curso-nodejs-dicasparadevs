479380f8a620aa394fb58b22139150424a38b1f6
matheushigino_nepreconsulting <matheus.higino@nepreconsulting.com>
2024-01-03 13:02:17 -0300

    Conectando-se ao MongoDB

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-  const connectionString =
-    "mongodb+srv://admin:<password>@cursonodejsdicasparadev.qprga4f.mongodb.net/?retryWrites=true&w=majority";
-  await mongoose.connect(connectionString, (error) => {
-    if (error) {
-      return console.log(
-        "Ocorreu um erro ao se conectar com o banco de dados: ",
-        error
-      );
-    }
-    return console.log("Conexão com o bano de dados realizada com sucesso");
-  });
+  const connectionString = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejsdicasparadev.qprga4f.mongodb.net/database?retryWrites=true&w=majority`;
+  //   await mongoose.connect(connectionString, (error) => {
+  //     if (error) {
+  //       return console.log(
+  //         "Ocorreu um erro ao se conectar com o banco de dados: ",
+  //         error
+  //       );
+  //     }
+  //     return console.log("Conexão com o bano de dados realizada com sucesso");
+  //   });
+  await mongoose
+    .connect(connectionString)
+    .then(() => console.log("Conexão bem-sucedida"))
+    .catch((error) => console.error("Erro na conexão:", error));
 };
 
 module.exports = connectToDatabase;
